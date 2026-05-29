25e72721fb459ffaddf6047cab9023e45ed3c860
pratap <rmstech25@gmail.com>
2026-05-29 16:10:13 +0530

    logu.png issue fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -517,7 +517,7 @@
 import Link from "next/link";
 import { useState } from "react";
 
-const basePath = process.env.NODE_ENV === "production" ? "/brightpathai-website" : "";
+const basePath = "";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
